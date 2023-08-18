2c8656809eec4be5ccb532086807e6fdcfbe77ce
Arsg61 <142576518+Arsg61@users.noreply.github.com>
2023-08-18 13:29:57 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,32 @@
-ydef
+import csv
+def addtxt() :
+    f=open("rrr.txt","a")
+    l=[]
+    while True :
+        st=input("enter string :")
+        f.write(st+"\n")
+        l.append(st)
+        y=input("do you want to continue (yes/no):")
+        if y=='no' :
+            break
+    print(l)
+    f.close()
+def readtxt():
+    f=open("rrr.txt","r")
+    st=f.readlines()
+    for line in st :
+        print(line,end='')
+    f.close()
+def txt() :
+    f=open("rrr.txt","w")
+    l=[]
+    while True :
+        st=input("enter string :")
+        f.write(st+"\n")
+        l.append(st)
+        y=input("do you want to continue (yes/no):")
+        if y=='no' :
+            break
+    print("data you have filled recently : ")
+    print( l)
+    f.close()
