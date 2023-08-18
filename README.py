578cee9c2c29ee20704354d37a197b78a5174f7d
Arsg61 <142576518+Arsg61@users.noreply.github.com>
2023-08-18 19:04:21 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,7 @@
 import csv
+import pickle as pic
 def addtxt() :
-    f=open("rrr.txt","a")
+    f=open("file.txt","a")
     l=[]
     while True :
         st=input("enter string :")
@@ -12,13 +13,13 @@ def addtxt() :
     print(l)
     f.close()
 def readtxt():
-    f=open("rrr.txt","r")
+    f=open("file.txt","r")
     st=f.readlines()
     for line in st :
         print(line,end='')
     f.close()
-def txt() :
-    f=open("rrr.txt","w")
+def writetxt() :
+    f=open("file.txt","w")
     l=[]
     while True :
         st=input("enter string :")
@@ -31,7 +32,7 @@ def txt() :
     print( l)
     f.close()
 def writrecsv() :
-    f=open("rrr.csv","a",newline="")
+    f=open("file.csv","w",newline="")
     w=csv.writer(f) 
     while True :
         n=input("enter name :")
@@ -43,11 +44,56 @@ def writrecsv() :
         if n=="yes" :
             break
     f.close()
-def addcsv()
-
-
-
-
-
-
-
+def addcsv() :
+    f=open("file.csv","a",newline="")
+    w=csv.writer(f) 
+    while True :
+        n=input("enter name :")
+        r=int(input("enter roll no. "))
+        m=int(input("enter marks :"))
+        l=[n,r,m]
+        w.writerow(l)
+        n=input("enter yes or no :")
+        if n=="yes" :
+            break
+    f.close()
+def readcsv() :
+    f=open("file.csv","r")
+    file=csv.reader(f)
+    for row in file :
+        print(row)
+    f.close()
+def writebin():
+    f=open("file.dat","wb")
+    while True :
+        n=input("enter name :")
+        r=int(input("enter roll no. "))
+        m=int(input("enter marks :"))
+        l={'Name':n,"Roll no.":r,"Marks":m}     
+        pic.dump(l,f)
+        c=input("do you want to continue (yes/no):")
+        if c=="no"  :
+            break
+    f.close()
+def addbin() :
+    f=open("file.dat","ab")
+    while True :
+        n=input("enter name :")
+        r=int(input("enter roll no. "))
+        m=int(input("enter marks :"))
+        l={'Name':n,"Roll no.":r,"Marks":m}     
+        pic.dump(l,f)
+        c=input("do you want to continue (yes/no):")
+        if c=="no"  :
+            break
+    f.close()
+def readbin() :
+    f=open("file.dat","rb")
+    while True :
+        try : 
+            d=pic.load(f)
+            print(d)
+        except EOFError:
+            break
+    f.close()
+  
